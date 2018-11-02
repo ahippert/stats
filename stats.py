4fbdf010a0689b1aaf794846ccc342164de31553
ahippert <hippert27@gmail.com>
2018-11-02 18:34:31 +0100

    Added two functions to compute spatial and temporal means

--- stats.py
+++ stats.py
@@ -3,13 +3,13 @@
 # Statistics in python
 #
 # AH 07-09-2018
-# Last update : 25-10-2018
+# Last update : 01-11-2018
 
 import numpy as np
 
-# compute the Root-Mean-Square Error (RMSE) between prediction and observation fields
-#
 def rmse(pred, obs, nt, nx, ny, col=True):
+    """ Compute the Root-Mean-Square Error (RMSE) 
+    between prediction and observation fields """
     sum_of_dif = 0
     for i in range(0, nt):
         if col :
@@ -18,33 +18,33 @@ def rmse(pred, obs, nt, nx, ny, col=True):
             sum_of_dif += np.linalg.norm(pred[i] - obs[i])**2
     return np.sqrt(sum_of_dif/(nt*nx*ny))
 
-# Compute RMSE between the mean of prediction and observation fields
-#
 def rmse_zero_mode(pred, obs, nt, nx, ny):
+    """ Compute RMSE between the mean of prediction and observation fields
+    """
     sum_of_dif = 0
     for i in range(0, nt):
         sum_of_dif += np.linalg.norm(np.mean(pred[:,i])-obs[:,i])**2
     return np.sqrt(sum_of_dif/(nt*nx*ny))
 
-# Compute cross-RMSE between prediction and observation fields
-#
 def rmse_cross_v(pred, obs, N):
+    """ Compute cross-RMSE between prediction and observation fields
+    """
     sum_of_dif = 0 
     for i in range(0, N):
         sum_of_dif += np.linalg.norm(pred[i] - obs[i])**2
     return np.sqrt(sum_of_dif/N)
 
-# Compute cross-RMSE between the mean of prediction and observation fields
-#
 def rmse_cv_zero_mode(pred, obs, N):
+    """ Compute cross-RMSE between the mean of prediction and observation fields
+    """
     sum_of_dif = 0 
     for i in range(0, N):
         sum_of_dif += np.linalg.norm(np.mean(pred) - obs[i])**2
     return np.sqrt(sum_of_dif/(t*N))
 
-# Compute RMSE between two images
-#
 def compute_image_rmse(im1, im2, nx, ny):
+    """ Compute RMSE between two images
+    """
     rmse_im = 0
     for i in range(nx):
         for j in range(ny):
@@ -64,9 +64,9 @@ def compute_mean(data, col, nozeros=True):
             datamean.append(np.nanmean(data[i]))
     return datamean
 
-# Compute the column/line mean of a matrix without zeros
-#
 def compute_mean0(matrix, n, col, nozeros=True):
+    '''Compute the column/line mean of a matrix without zeros
+    '''
     matrix_mean = []
     for i in range(0, n):
         if col:
@@ -81,45 +81,59 @@ def compute_mean0(matrix, n, col, nozeros=True):
                 matrix_mean.append(np.mean(matrix[i]))
     return matrix_mean
     
-# Remove the column/line mean of a matrix
-#
 def remove_mean(data, mean, col):
-    if data.shape[0]>data.shape[1]:
-        n=data.shape[1]
-    else:
-        n=data.shape[0]
+    """ Remove the column/line mean of a matrix
+    """
     if col: 
-        for i in range(0, n):
+        for i in range(0, data.shape[1]):
             data[:,i] -= mean[i]
     else:
-        for i in range(0, n):
+        for i in range(0, data.shape[0]):
             data[i] -= mean[i]
     return data
 
-# Add the column/line mean of a matrix
-#
+def remove_t_mean(data, mean):
+    """ Remove the temporal mean of a time series matrix
+    """
+    n = max(data.shape[0],data.shape[1])
+    for i in range(0, n):
+        if data.shape[0]==n:
+            data[i] -= mean[i]
+        else :
+            data[:,i] -= mean[i]
+    return data
+
+def remove_s_mean(data, mean):
+    """ Remove the spatial mean of a time series matrix
+    """
+    n = min(data.shape[0],data.shape[1])
+    for i in range(0, n):
+        if data.shape[0]==n:
+            data[i] -= mean[i]
+        else :
+            data[:,i] -= mean[i]
+    return data
+
 def add_mean(data, mean, col):
-    if data.shape[0]>data.shape[1]:
-        n=data.shape[1]
-    else:
-        n=data.shape[0]
+    """ Add the column/line mean of a matrix 
+    """
     if col:
-        for i in range(0, n):
+        for i in range(0, data.shape[1]):
             if np.isnan(mean[i]):
                 data[:,i] += 0.
             else:
                 data[:,i] += mean[i]
     else:
-        for i in range(0, n):
+        for i in range(0, data.shape[0]):
             if np.isnan(mean[i]):
                 data[i] += 0. 
             else:
                 data[i] += mean[i]
     return data
 
-# Compute statistical Chi-2 between observations and predictions
-#
 def chi2(obs, pred, n):
+    """ Compute statistical Chi-2 between observations and predictions
+    """
     chi = 0.
     for i in range(n):
         if pred[i]==0. or obs[i]==0. or (pred[i]==0. and obs[i]!=0.):
@@ -128,9 +142,9 @@ def chi2(obs, pred, n):
             chi += (1/np.var(obs))*(obs[i] - pred[i])**2
     return chi
 
-# Compute the F-Test between the variances of two datasets
-#
 def ftest(data1, data2):
+    """ Compute the F-Test between the variances of two datasets
+    """
     var1, var2 = np.var(data1), np.var(data2)
     if var1 > var2:
         return var1/var2
